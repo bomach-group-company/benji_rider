255e1e52b57cba7bffff8bca08eb29b43116190c
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-07-26 10:33:01 +0100

    Added authentication files

---
+++ lib/src/providers/signup controller.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+import '../services/repos/authentication_repo.dart';
+
+class SignupController extends GetxController {
+  static SignupController get instance => Get.find();
+
+  //TextFormField Controllers
+  final userFirstNameEC = TextEditingController();
+  final userLastNameEC = TextEditingController();
+  final userEmailEC = TextEditingController();
+  final userPhoneNumberEC = TextEditingController();
+  final userPasswordEC = TextEditingController();
+
+  void registerUser(String userEmailEC, String userPasswordEC) {
+    AuthenticationRepo.instance
+        .createUserWithEmailAndPassword(userEmailEC, userPasswordEC);
+  }
+}
