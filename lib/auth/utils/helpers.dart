cc521d0064bbf835124983a471b0538e686cc182
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-08-14 17:05:11 +0100

    added helpers functions for working with the api

---
+++ lib/auth/utils/helpers.dart
@@ -0,0 +1,34 @@
+import 'package:benji_rider/app/auth/login.dart';
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> saveAuthToken(String authToken) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('auth_token', authToken);
+}
+
+Future<String?> getAuthToken() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getString('auth_token');
+}
+
+Map<String, String> authHeader(authToken) =>
+    {'Authorization': 'Bearer $authToken', 'Content-Type': 'application/json'};
+
+dynamic isUnauthorized(Map data) {
+  if (data.containsKey('detail') && data['detail'] == 'Unauthorized') {
+    return Get.offAll(
+      () => Login(),
+      routeName: 'Login',
+      predicate: (route) => false,
+      duration: const Duration(milliseconds: 300),
+      fullscreenDialog: true,
+      curve: Curves.easeIn,
+      popGesture: false,
+      transition: Transition.rightToLeft,
+    );
+  } else {
+    return data;
+  }
+}
