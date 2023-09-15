55c780420490e1cfa038a8ad269b2c19c4167409
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-09-15 14:44:52 +0100

    changed the vendor scroll to use a faster method

--- lib/app/vendors/vendors.dart
+++ lib/app/vendors/vendors.dart
@@ -27,6 +27,43 @@ class _VendorsState extends State<Vendors> with SingleTickerProviderStateMixin {
   void initState() {
     super.initState();
     checkAuth(context);
+    _getData();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  Future<void> _scrollListener() async {
+    if (_scrollController.position.pixels >= 200 &&
+        _isScrollToTopBtnVisible != true) {
+      setState(() {
+        _isScrollToTopBtnVisible = true;
+      });
+    }
+    if (_scrollController.position.pixels < 200 &&
+        _isScrollToTopBtnVisible == true) {
+      setState(() {
+        _isScrollToTopBtnVisible = false;
+      });
+    }
+  }
+
+  Future<void> _scrollToTop() async {
+    await _scrollController.animateTo(
+      0.0,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+    setState(() {
+      _isScrollToTopBtnVisible = false;
+    });
+  }
+
+  List<VendorModel>? _data;
+
+  Future<void> _getData() async {
+    List<VendorModel> data = await getVendors();
+    setState(() {
+      _data = data;
+    });
   }
 
   @override
@@ -42,8 +79,6 @@ class _VendorsState extends State<Vendors> with SingleTickerProviderStateMixin {
 
   bool _isScrollToTopBtnVisible = false;
   final String _vendorsImage = "ntachi-osa";
-
-  final int _vendors = 15;
 
   // final String _vendorActive = "Online";
   // final Color _vendorActiveColor = kSuccessColor;
@@ -68,29 +103,6 @@ class _VendorsState extends State<Vendors> with SingleTickerProviderStateMixin {
 
   Future<void> _handleRefresh() async {
     setState(() {});
-  }
-
-//============================= Scroll to Top ======================================//
-  void _scrollToTop() {
-    _animationController.reverse();
-    _scrollController.animateTo(
-      0,
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
-    );
-  }
-
-  void _scrollListener() {
-    //========= Show action button ========//
-    if (_scrollController.position.pixels >= 200) {
-      _animationController.forward();
-      setState(() => _isScrollToTopBtnVisible = true);
-    }
-    //========= Hide action button ========//
-    else if (_scrollController.position.pixels < 200) {
-      _animationController.reverse();
-      setState(() => _isScrollToTopBtnVisible = true);
-    }
   }
 
 //=============================== See more ========================================\\
@@ -130,202 +142,170 @@ class _VendorsState extends State<Vendors> with SingleTickerProviderStateMixin {
             ),
           ],
         ),
-        floatingActionButton: Stack(
-          children: <Widget>[
-            if (_isScrollToTopBtnVisible) ...[
-              ScaleTransition(
-                scale: CurvedAnimation(
-                  parent: _animationController,
-                  curve: Curves.easeInOut,
-                ),
-                child: FloatingActionButton(
-                  onPressed: _scrollToTop,
-                  mini: true,
-                  backgroundColor: kAccentColor,
-                  enableFeedback: true,
-                  mouseCursor: SystemMouseCursors.click,
-                  tooltip: "Scroll to top",
-                  hoverColor: kAccentColor,
-                  hoverElevation: 50.0,
-                  child: const Icon(Icons.keyboard_arrow_up),
-                ),
-              ),
-            ]
-          ],
-        ),
+        floatingActionButton: _isScrollToTopBtnVisible
+            ? FloatingActionButton(
+                onPressed: _scrollToTop,
+                mini: true,
+                backgroundColor: kAccentColor,
+                enableFeedback: true,
+                mouseCursor: SystemMouseCursors.click,
+                tooltip: "Scroll to top",
+                hoverColor: kAccentColor,
+                hoverElevation: 50.0,
+                child: const Icon(Icons.keyboard_arrow_up),
+              )
+            : SizedBox(),
         extendBody: true,
         extendBodyBehindAppBar: true,
         body: SafeArea(
           maintainBottomViewPadding: true,
-          child: FutureBuilder(
-            future: getVendors(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const AllVendorsPageSkeleton();
-              }
-              if (snapshot.data == null) {
-                return const AllVendorsPageSkeleton();
-              }
-              if (snapshot.connectionState == ConnectionState.none) {
-                return const Center(
-                  child: Text("Please connect to the internet"),
-                );
-              }
-              // if (snapshot.connectionState == snapshot.requireData) {
-              //   SpinKitDoubleBounce(color: kAccentColor);
-              // }
-              if (snapshot.connectionState == snapshot.error) {
-                return const Center(
-                  child: Text("Error, Please try again later"),
-                );
-              }
-              return Scrollbar(
-                controller: _scrollController,
-                radius: const Radius.circular(10),
-                scrollbarOrientation: ScrollbarOrientation.right,
-                child: ListView(
+          child: _data == null
+              ? AllVendorsPageSkeleton()
+              : Scrollbar(
                   controller: _scrollController,
-                  physics: const BouncingScrollPhysics(),
-                  padding: const EdgeInsets.all(kDefaultPadding / 2),
-                  children: [
-                    ListView.separated(
-                      separatorBuilder: (context, index) =>
-                          const SizedBox(height: kDefaultPadding / 2),
-                      itemCount: snapshot.data!.length,
-                      addAutomaticKeepAlives: true,
-                      physics: const BouncingScrollPhysics(),
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) => InkWell(
-                        onTap: _viewVendor,
-                        borderRadius: BorderRadius.circular(16),
-                        child: Container(
-                          decoration: ShapeDecoration(
-                            color: kPrimaryColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                            shadows: const [
-                              BoxShadow(
-                                color: Color(0x0F000000),
-                                blurRadius: 24,
-                                offset: Offset(0, 4),
-                                spreadRadius: 0,
+                  radius: const Radius.circular(10),
+                  scrollbarOrientation: ScrollbarOrientation.right,
+                  child: ListView(
+                    controller: _scrollController,
+                    physics: const BouncingScrollPhysics(),
+                    padding: const EdgeInsets.all(kDefaultPadding / 2),
+                    children: [
+                      ListView.separated(
+                        separatorBuilder: (context, index) =>
+                            const SizedBox(height: kDefaultPadding / 2),
+                        itemCount: _data!.length,
+                        addAutomaticKeepAlives: true,
+                        physics: const BouncingScrollPhysics(),
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) => InkWell(
+                          onTap: _viewVendor,
+                          borderRadius: BorderRadius.circular(16),
+                          child: Container(
+                            decoration: ShapeDecoration(
+                              color: kPrimaryColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16),
                               ),
-                            ],
-                          ),
-                          child: Row(
-                            children: [
-                              Container(
-                                width: 130,
-                                height: 130,
-                                decoration: ShapeDecoration(
-                                  color: kPageSkeletonColor,
-                                  image: DecorationImage(
-                                    image: AssetImage(
-                                      "assets/images/vendors/$_vendorsImage.png",
+                              shadows: const [
+                                BoxShadow(
+                                  color: Color(0x0F000000),
+                                  blurRadius: 24,
+                                  offset: Offset(0, 4),
+                                  spreadRadius: 0,
+                                ),
+                              ],
+                            ),
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 130,
+                                  height: 130,
+                                  decoration: ShapeDecoration(
+                                    color: kPageSkeletonColor,
+                                    image: DecorationImage(
+                                      image: AssetImage(
+                                        "assets/images/vendors/$_vendorsImage.png",
+                                      ),
+                                      fit: BoxFit.fill,
                                     ),
-                                    fit: BoxFit.fill,
-                                  ),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(16),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(16),
+                                    ),
                                   ),
                                 ),
-                              ),
-                              kHalfWidthSizedBox,
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(
-                                    width: mediaWidth - 200,
-                                    child: Text(
-                                      snapshot.data![index].shopName!,
-                                      overflow: TextOverflow.ellipsis,
-                                      maxLines: 1,
-                                      style: const TextStyle(
-                                        color: kBlackColor,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w700,
-                                        letterSpacing: -0.36,
+                                kHalfWidthSizedBox,
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      width: mediaWidth - 200,
+                                      child: Text(
+                                        _data![index].shopName!,
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 1,
+                                        style: const TextStyle(
+                                          color: kBlackColor,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w700,
+                                          letterSpacing: -0.36,
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                  kHalfSizedBox,
-                                  Row(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      InkWell(
-                                        onTap: _viewVendor,
-                                        child: Container(
-                                          margin: EdgeInsets.only(top: 1),
-                                          child: FaIcon(
-                                            FontAwesomeIcons.locationDot,
-                                            color: kAccentColor,
-                                            size: 21,
+                                    kHalfSizedBox,
+                                    Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        InkWell(
+                                          onTap: _viewVendor,
+                                          child: Container(
+                                            margin: EdgeInsets.only(top: 1),
+                                            child: FaIcon(
+                                              FontAwesomeIcons.locationDot,
+                                              color: kAccentColor,
+                                              size: 21,
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                      SizedBox(width: 5),
-                                      SizedBox(
-                                        width: mediaWidth - 200,
-                                        child: Text(
-                                          "${snapshot.data![index].address ?? 'Not Available'}",
-                                          overflow: TextOverflow.ellipsis,
-                                          maxLines: 3,
-                                          style: TextStyle(
-                                            color: kTextBlackColor,
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.w500,
+                                        SizedBox(width: 5),
+                                        SizedBox(
+                                          width: mediaWidth - 200,
+                                          child: Text(
+                                            "${_data![index].address ?? 'Not Available'}",
+                                            overflow: TextOverflow.ellipsis,
+                                            maxLines: 3,
+                                            style: TextStyle(
+                                              color: kTextBlackColor,
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w500,
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                    ],
-                                  ),
-                                  kHalfSizedBox,
-                                  Row(
-                                    children: [
-                                      FaIcon(
-                                        FontAwesomeIcons.boxOpen,
-                                        size: 15,
-                                        color: kAccentColor,
-                                      ),
-                                      SizedBox(width: 5),
-                                      SizedBox(
-                                        width: mediaWidth - 200,
-                                        child: Text(
-                                          "${formattedText(_totalNumberOfOrders)} orders",
-                                          overflow: TextOverflow.ellipsis,
-                                          maxLines: 1,
-                                          style: TextStyle(
-                                            color: kTextBlackColor,
-                                            fontSize: 15,
-                                            fontWeight: FontWeight.w700,
-                                            letterSpacing: -0.24,
+                                      ],
+                                    ),
+                                    kHalfSizedBox,
+                                    Row(
+                                      children: [
+                                        FaIcon(
+                                          FontAwesomeIcons.boxOpen,
+                                          size: 15,
+                                          color: kAccentColor,
+                                        ),
+                                        SizedBox(width: 5),
+                                        SizedBox(
+                                          width: mediaWidth - 200,
+                                          child: Text(
+                                            "${formattedText(_totalNumberOfOrders)} orders",
+                                            overflow: TextOverflow.ellipsis,
+                                            maxLines: 1,
+                                            style: TextStyle(
+                                              color: kTextBlackColor,
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w700,
+                                              letterSpacing: -0.24,
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ],
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    kSizedBox,
-                    TextButton(
-                      onPressed: _seeMoreOnlineVendors,
-                      child: Text(
-                        "See more",
-                        style: TextStyle(color: kAccentColor),
+                      kSizedBox,
+                      TextButton(
+                        onPressed: _seeMoreOnlineVendors,
+                        child: Text(
+                          "See more",
+                          style: TextStyle(color: kAccentColor),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              );
-            },
-          ),
         ),
       ),
     );
