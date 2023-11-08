daceda73d9b4dd1b156db0edc325582cde1cd6dd
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-11-08 15:36:49 +0100

    added withdrawal again

--- lib/app/settings/settings.dart
+++ lib/app/settings/settings.dart
@@ -1,3 +1,4 @@
+import 'package:benji_rider/app/withdrawal/withdraw_history.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 
@@ -79,99 +80,6 @@ class _SettingsPageState extends State<SettingsPage> {
           scrollDirection: Axis.vertical,
           children: [
             ProfileFirstHalf(availableBalance: _accountBalance),
-            // Padding(
-            //   padding: const EdgeInsets.only(
-            //     top: kDefaultPadding,
-            //     left: kDefaultPadding,
-            //     right: kDefaultPadding,
-            //     bottom: kDefaultPadding / 1.5,
-            //   ),
-            //   child: Container(
-            //     padding: const EdgeInsets.all(
-            //       kDefaultPadding / 2,
-            //     ),
-            //     decoration: ShapeDecoration(
-            //       color: kPrimaryColor,
-            //       shape: RoundedRectangleBorder(
-            //         borderRadius: BorderRadius.circular(15),
-            //       ),
-            //       shadows: const [
-            //         BoxShadow(
-            //           color: Color(0x0F000000),
-            //           blurRadius: 24,
-            //           offset: Offset(0, 4),
-            //           spreadRadius: 0,
-            //         ),
-            //       ],
-            //     ),
-            //     child: Column(
-            //       children: [
-            //         ListTile(
-            //           onTap: () {
-            //             Get.to(
-            //               () => const Earning(),
-            //               routeName: 'Earning',
-            //               duration: const Duration(milliseconds: 300),
-            //               fullscreenDialog: true,
-            //               curve: Curves.easeIn,
-            //               preventDuplicates: true,
-            //               popGesture: true,
-            //               transition: Transition.rightToLeft,
-            //             );
-            //           },
-            //           leading: Icon(
-            //             Icons.money,
-            //             color: kAccentColor,
-            //           ),
-            //           title: const Text(
-            //             'Earning',
-            //             style: TextStyle(
-            //               color: Color(
-            //                 0xFF333333,
-            //               ),
-            //               fontSize: 15,
-            //               fontWeight: FontWeight.w400,
-            //             ),
-            //           ),
-            //           trailing: const Icon(
-            //             Icons.arrow_forward_ios_rounded,
-            //           ),
-            //         ),
-            //         ListTile(
-            //           onTap: () {
-            //             Get.to(
-            //               () => const WithdrawHistoryPage(),
-            //               routeName: 'WithdrawHistoryPage',
-            //               duration: const Duration(milliseconds: 300),
-            //               fullscreenDialog: true,
-            //               curve: Curves.easeIn,
-            //               preventDuplicates: true,
-            //               popGesture: true,
-            //               transition: Transition.rightToLeft,
-            //             );
-            //           },
-            //           leading: Icon(
-            //             Icons.history,
-            //             color: kAccentColor,
-            //           ),
-            //           title: const Text(
-            //             'Withdrawal History',
-            //             style: TextStyle(
-            //               color: Color(
-            //                 0xFF333333,
-            //               ),
-            //               fontSize: 15,
-            //               fontWeight: FontWeight.w400,
-            //             ),
-            //           ),
-            //           trailing: const Icon(
-            //             Icons.arrow_forward_ios_rounded,
-            //           ),
-            //         ),
-            //       ],
-            //     ),
-            //   ),
-            // ),
             Padding(
               padding: const EdgeInsets.only(
                 top: kDefaultPadding / 1.5,
@@ -199,6 +107,37 @@ class _SettingsPageState extends State<SettingsPage> {
                 ),
                 child: Column(
                   children: [
+                    ListTile(
+                      onTap: () {
+                        Get.to(
+                          () => const WithdrawHistoryPage(),
+                          routeName: 'WithdrawHistoryPage',
+                          duration: const Duration(milliseconds: 300),
+                          fullscreenDialog: true,
+                          curve: Curves.easeIn,
+                          preventDuplicates: true,
+                          popGesture: true,
+                          transition: Transition.rightToLeft,
+                        );
+                      },
+                      leading: Icon(
+                        Icons.history,
+                        color: kAccentColor,
+                      ),
+                      title: const Text(
+                        'Withdrawal History',
+                        style: TextStyle(
+                          color: Color(
+                            0xFF333333,
+                          ),
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios_rounded,
+                      ),
+                    ),
                     ListTile(
                       onTap: _toDeliveryPage,
                       leading: Icon(
