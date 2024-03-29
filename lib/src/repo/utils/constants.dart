5ccb15bdfd785a606532924eca72f0a244189d63
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-03-29 17:17:21 +0100

    ran flutterfire configure

--- lib/src/repo/utils/constants.dart
+++ lib/src/repo/utils/constants.dart
@@ -1,3 +1,4 @@
 const String baseURL = 'https://resource.bgbot.app/api/v1';
 const String baseImage = "https://resource.bgbot.app";
-const String websocketBaseUrl = "ws://wsbenji.bgbot.app/ws";
+const String websocketBaseUrl =
+    "ws://wsbenji.bgbot.app/ws"; // the issue is that it is using ws on the backend instead of wss - "wss://wsbenji.bgbot.app/ws"
