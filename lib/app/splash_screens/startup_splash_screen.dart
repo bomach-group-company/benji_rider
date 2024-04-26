d5c2c7ef633f580ad7288e69390b3ca992011680
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-04-26 09:16:41 +0100

    fixed  issue with handle FCM

--- lib/app/splash_screens/startup_splash_screen.dart
+++ lib/app/splash_screens/startup_splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:benji_rider/src/repo/controller/fcm_messaging_controller.dart';
 import 'package:benji_rider/src/repo/controller/package_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -24,6 +25,8 @@ class _StartupSplashscreenState extends State<StartupSplashscreen> {
   @override
   void initState() {
     super.initState();
+    FcmMessagingController.instance.handleFCM();
+
     UserController.instance.ifUser().then((value) {
       if (value) {
         OrderController.instance.getOrdersByStatus();
