937aba7a3769902e5f07a6e4da7c26eaf17faebd
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-06-19 17:06:14 +0100

    changed the url

--- lib/src/repo/controller/api_url.dart
+++ lib/src/repo/controller/api_url.dart
@@ -7,8 +7,8 @@ import 'package:http/http.dart' as http;
 import '../controller/user_controller.dart';
 
 // API URLS AND HTTP CALL FUNCTIONS
-const baseURL = "http://resource.bgbot.app/api/v1";
-const baseImage = "http://resource.bgbot.app";
+const baseURL = "https://resource.bgbot.app/api/v1";
+const baseImage = "https://resource.bgbot.app";
 
 const String websocketBaseUrl =
     "ws://wsbenji.bgbot.app/ws"; // the issue is that it is using ws on the backend instead of wss - "wss://wsbenji.bgbot.app/ws"
@@ -19,7 +19,7 @@ const String websocketBaseUrl =
 var vendorId = UserController.instance.user.value.id;
 
 class Api {
-  static const baseUrl = "http://resource.bgbot.app/api/v1";
+  static const baseUrl = "https://resource.bgbot.app/api/v1";
   static const login = "/auth/token";
   static const user = "/auth/";
   static const changePassword = "/auth/changeNewPassword/";
